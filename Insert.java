5d68c200f153a56823439ccfbef9d6ba89af8f92
sravanthi-vulavabeeti <168511844+sravanthi-vulavabeeti@users.noreply.github.com>
2025-06-17 09:56:30 +0530

    Add files via upload

---
+++ Insert.java
@@ -0,0 +1,25 @@
+class Insert {
+    static int binarySearch(int arr[], int low, int high,
+                            int key)
+    {
+        if (high < low)
+            return -1;
+        
+        int mid = (low + high) / 2;
+        if (key == arr[mid])
+            return mid;
+        if (key > arr[mid])
+            return binarySearch(arr, (mid + 1), high, key);
+        return binarySearch(arr, low, (mid - 1), key);
+    }
+    
+    public static void main(String[] args)
+    {
+        int arr[] = { 5, 6, 7, 8, 9, 10 };
+        int n, key;
+        n = arr.length - 1;
+        key = 10;
+        System.out.println("Index: "
+                           + binarySearch(arr, 0, n, key));
+    }
+}
