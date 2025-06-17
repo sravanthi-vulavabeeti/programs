75456a27bd71afdaa0f7abfc74a14a8a5a85cf01
sravanthi-vulavabeeti <168511844+sravanthi-vulavabeeti@users.noreply.github.com>
2025-06-17 10:44:41 +0530

    Add files via upload

---
+++ Postorder.java
@@ -0,0 +1,36 @@
+class Postorder{
+    int data;
+    Node left, right;
+
+    public Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinaryTree {
+    Node root;
+
+    void postorderTraversal(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        postorderTraversal(node.left);
+        postorderTraversal(node.right);
+        System.out.print(node.data + " ");
+    }
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+
+        System.out.println("Postorder traversal of the binary tree is:");
+        tree.postorderTraversal(tree.root);
+    }
+}
